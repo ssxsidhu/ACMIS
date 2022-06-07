00da01915b4796084c430efd4d59fd554a66f3dc
CyberWolf0000 <106175710+CyberWolf0000@users.noreply.github.com>
2022-06-07 16:05:34 -0500

    Add files via upload
    Uploaded New Version of Class which was built in Android Studio. Previous File in Commit was written without any build tools

---
+++ app/src/main/java/comp3350/acmis/objects/Flight.java
@@ -0,0 +1,100 @@
+package comp3350.acmis.objects;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.time.*;
+
+public class Flight{
+
+    // STATIC VARIABLE
+    private static int flight_ID = 0;
+
+    // Instance Variables
+    private int flightSerial;
+    private String startDestination;
+    private String endDestination;
+    private double distance;                    // This needs to be pulled from data base.
+    private double cost;                        // Airline will assign this value in constructor. Airline decides ticket prices.
+
+    private Airplane assigned;                  // Assigned Airplane to this flight. Important for performing checks and what not.
+    private LocalDateTime departureTime;        // Store the departure time of the flight in a DateTime Format.
+    private LocalDateTime estimatedTime;        // Same as above except for arrivals
+
+    // Constructor
+    public Flight(String newStart, String newEnd, double newDistance, double newCost){
+
+        flightSerial = flight_ID;
+        startDestination = newStart;
+        endDestination = newEnd;
+        distance = newDistance;         // Value must be supplied from Data base. 
+        cost = newCost;                 // Value supplied from Airline
+
+        flight_ID+=2;                   // FLights going to are EVEN. Flights Coming from are ODD. If NO odd exists there is no return flight.
+    }
+
+    
+    // Setters()
+    private void setFlightID()          // Decrement so that new flight ID becomes ODD. This METHOD IS CALLED ONLY AND ONLY WHEN RETURNFLIGHT() IS CALLED
+    {flightSerial--;}
+    public void setPrice(double newPrice)   // Duh.
+    {cost = newPrice;}
+
+    // Assign an Aircraft for this flight. Perform Checks before accepting.
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void assignPlane(Airplane newPlane){
+
+        // Perform check on newPlane for range. Do something only if plane can travel that far.
+        if(newPlane.getRange()>distance){
+
+            assigned = newPlane;        
+            estimatedTime = departureTime.plusHours((int)(distance/assigned.getSpeed()));           // Calculate Estimated Time based on plane attributes.
+        }
+        else
+            System.out.println("ERROR. PLANE RANGE LESSER THAN FLIGHT DISTANCE.\n");        
+    }
+
+    // Change the depart time just because. We will also change arrivalTime
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void setDepartTime(LocalDateTime newTime)
+    {
+        departureTime = newTime;
+        estimatedTime = departureTime.plusHours((int)(distance/assigned.getSpeed()));       // Change arrival time accordingly based on distance and plane speed.
+    }
+
+    // RETURN FLIGHT. CREATE NEW OBJECT AND RETURN.
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public Flight createReturn(int returnAfter){                    // PARAMS. RETURN AFTER THESE MANY HOURS if returnAfter = 3, then plane departs 3 hours after its arrival time.
+
+        Flight returnFlight = new Flight(endDestination, startDestination, distance, cost);
+        returnFlight.assignPlane(assigned);
+
+        returnFlight.setDepartTime(estimatedTime.plusHours(returnAfter));
+        returnFlight.setFlightID();
+
+        return returnFlight;
+    }
+    
+    // LINK FLIGHT. IMPORTANT FOR MULTI ROUTES AND RETURN BOOKINGS OR EDITS. I FEEL THIS WILL BE NEEDED ANYWAY
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void linkFlight(Flight linkThis, LocalDateTime linkThisDepartTime){
+
+        if(this.endDestination.equals(linkThis.startDestination)){                         // Do Something only if arrival dest of flight 1 is same as depart dest of flight 2          
+
+            if(linkThisDepartTime.isAfter(estimatedTime))                                   // Do Something only if Depart Time for flight 2 is AFTER arrival time of flight 1
+            {
+                        System.out.println();/* NEED TO THINK. API wont allow to Commit unless I write something here*/
+            }
+            else
+            System.out.println("ERROR. Second Flight CANNOT leave BEFORE ARRIVAL of Current Flight !\n");
+        }
+        else
+        {System.out.println("ERROR. CANNOT LINK FLIGHT. Destination of Current is NOT same AS DEPARTURE for Flight Entered in Params\n");}
+    }
+    
+    public String getFlightDetails()            // Can also rename to toString()
+    {
+        return ("Flight ID: "+flightSerial+" \n"+startDestination+" ----> "+endDestination+"\n"+"Departure :"+departureTime+" ----> "+estimatedTime+" \n"+"Distance: "+distance+" km.");
+    }
+
+}
