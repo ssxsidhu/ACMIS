432532e33ecdd813e0bb4591f28ed6dcdcb6f58a
sahil-sidhu <96554179+sahil-sidhu@users.noreply.github.com>
2022-06-10 01:56:30 -0500

    Add basic structure to user class and made a userTest class to test it

--- app/src/main/java/comp3350/acmis/objects/User.java
+++ app/src/main/java/comp3350/acmis/objects/User.java
@@ -1,4 +1,44 @@
 package comp3350.acmis.objects;
 
+import java.util.Objects;
+
 public class User {
+    private String firstName, lastName, gender, username, password, email;
+    private long phoneNumber;
+
+    //constructor
+    //I have made all the variables require non-null values
+    // TODO: I have to ask prof If I can do that and test it using a different method as shown in testClass
+
+    public User(String firstName, String lastName, String gender, String username, String password, String email, String phoneNumber) {
+
+        this.firstName = Objects.requireNonNull(firstName, "First name cannot be null");
+        this.lastName = Objects.requireNonNull(lastName, "Last name cannot be null");
+        this.gender = Objects.requireNonNull(gender, "Gender cannot be null");
+        this.username = Objects.requireNonNull(username, "username cannot be null");
+        this.password = Objects.requireNonNull(password, "password cannot be null");
+        this.email = Objects.requireNonNull(email, "email cannot be null");
+        if(phoneNumber.length() < 10){
+            throw new IllegalArgumentException("Number should contain 10 digits");
+        }
+        else{
+            this.phoneNumber = Long.parseLong(Objects.requireNonNull(phoneNumber,"Phone number should not be null"));
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
