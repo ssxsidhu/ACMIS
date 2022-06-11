76e3172b3b0136a1661c57304764800d74f6e9f4
sahil-sidhu <96554179+sahil-sidhu@users.noreply.github.com>
2022-06-10 22:25:54 -0500

    Added activity for tabs namely, book flight and manage flights (google tabbed activity template used). Also updated the gif and theme of the app

--- app/src/main/java/comp3350/acmis/persistence/DataAccessStub.java
+++ app/src/main/java/comp3350/acmis/persistence/DataAccessStub.java
@@ -1,4 +1,15 @@
 package comp3350.acmis.persistence;
 
+import java.util.ArrayList;
+
+import comp3350.acmis.objects.Flight;
+import comp3350.acmis.objects.User;
+
 public class DataAccessStub {
+    private String dbName,dbType =  "stub";
+
+    private ArrayList<User> users;
+    private ArrayList<Flight> flights;
+
+
 }
