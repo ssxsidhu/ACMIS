2fe2c692112745264a5f6cb97d521b36e753e668
braden <bileskib@myumanitoba.ca>
2022-06-15 15:28:30 -0500

    user constructor changes and tests

--- app/src/main/java/comp3350/acmis/objects/User.java
+++ app/src/main/java/comp3350/acmis/objects/User.java
@@ -25,15 +25,18 @@ public class User {
 
     //constructor
     public User(String firstName, String lastName, Gender whichGender, String username, String password, String email, String phoneNumber) {
+        final String EMPTY_STRING = "";
 
-        this.firstName = Objects.requireNonNull(firstName, "First name cannot be null");
-        this.lastName = Objects.requireNonNull(lastName, "Last name cannot be null");
+        this.firstName = checks(firstName, "First name");
+        this.lastName = checks(lastName, "Last name");
         this.gender = Objects.requireNonNull(whichGender, "Gender cannot be null");
-        this.username = Objects.requireNonNull(username, "username cannot be null");
+        this.username = checks(username, "Username");
+
         this.password = Objects.requireNonNull(password, "password cannot be null");
         this.email = Objects.requireNonNull(email, "email cannot be null");
-        if(phoneNumber.length() < 10){
-            throw new IllegalArgumentException("Number should contain 10 digits");
+
+        if(phoneNumber.length() != 10){
+            throw new IllegalArgumentException("Phone number should contain 10 digits");
         }
         else{
             this.phoneNumber = Long.parseLong(Objects.requireNonNull(phoneNumber,"Phone number should not be null"));
@@ -55,6 +58,8 @@ public class User {
     // GETTERS
 
     public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+    public Gender getGender() {return gender;}
     public String getUsername() {return username;}
 
     public String getMyBookings(ArrayList<Booking> myBookings){
@@ -62,9 +67,17 @@ public class User {
         return null;
     }
 
+    private String checks(String value, String message) {
+        if (value.trim().equals("")){
+            throw new IllegalArgumentException(message + " can not be empty");
+        }
+        return Objects.requireNonNull(value, message + " cannot be null").trim();
+    }
+
     public enum Gender{
         MALE,
-        FEMALE
+        FEMALE,
+        OTHER
     }
 
 }
--- app/src/test/java/comp3350/acmis/objects/UserTest.java
+++ app/src/test/java/comp3350/acmis/objects/UserTest.java
@@ -2,21 +2,138 @@ package comp3350.acmis.objects;
 
 import static org.junit.Assert.*;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class UserTest {
-    @Rule
-    public ExpectedException expectedEx = ExpectedException.none();
-    //this is basic test we will a lot more tests
-    //Also I gotta check with the prof the use of @Rule.
     @Test
-    public void testConstructor() {
-        expectedEx.expect(RuntimeException.class);
-        expectedEx.expectMessage("Last name cannot be null");
+    public void testFirstName() {
+        User user = null;
 
-        User testUser1 = new User("foo", null,User.Gender.MALE , "fb", "fbp", "fb@gmail.com", "1111111111");
-        assertEquals("foo",testUser1.getFirstName());
+        //null firstname
+        try {
+            user = new User(null, "bar", User.Gender.MALE , "myUsername", "fbp", "fb@gmail.com", "1111111111");
+            fail("Expected a NullPointerException");
+        } catch (NullPointerException unused) {}
+
+        //empty firstname
+        try {
+            user = new User("", "bar", User.Gender.MALE , "myUsername", "fbp", "fb@gmail.com", "1111111111");
+            fail("Expected a IllegalArgumentException");
+        } catch (IllegalArgumentException unused) {}
+
+        //check only spaces
+        try {
+            user = new User("      ", "bar", User.Gender.MALE , "myUsername", "fbp", "fb@gmail.com", "1111111111");
+            fail("Expected a IllegalArgumentException");
+        } catch (IllegalArgumentException unused) {}
+
+        user = new User("foo", "bar", User.Gender.MALE , "myUsername", "fbp", "fb@gmail.com", "1111111111");
+        assertEquals("foo", user.getFirstName());
+
+        user = new User("    foo     ", "bar", User.Gender.MALE , "myUsername", "fbp", "fb@gmail.com", "1111111111");
+        assertEquals("foo", user.getFirstName());
     }
+
+    @Test
+    public void testLastName() {
+        User user = null;
+
+        //null lastname
+        try {
+            user = new User("foo", null, User.Gender.MALE , "myUsername", "fbp", "fb@gmail.com", "1111111111");
+            fail("Expected a NullPointerException");
+        } catch (NullPointerException unused) {}
+
+        //empty lastname
+        try {
+            user = new User("foo", "", User.Gender.MALE , "myUsername", "fbp", "fb@gmail.com", "1111111111");
+            fail("Expected a IllegalArgumentException");
+        } catch (IllegalArgumentException unused) {}
+
+        //check only spaces
+        try {
+            user = new User("foo", "      ", User.Gender.MALE , "myUsername", "fbp", "fb@gmail.com", "1111111111");
+            fail("Expected a IllegalArgumentException");
+        } catch (IllegalArgumentException unused) {}
+
+        user = new User("foo", "bar", User.Gender.MALE , "myUsername", "fbp", "fb@gmail.com", "1111111111");
+        assertEquals("bar", user.getLastName());
+
+        user = new User("    foo     ", "   bar     ", User.Gender.MALE , "myUsername", "fbp", "fb@gmail.com", "1111111111");
+        assertEquals("bar", user.getLastName());
+    }
+
+    @Test
+    public void testGender() {
+        User user = null;
+
+        //null gender
+        try {
+            user = new User("foo", "bar", null , "myUsername", "fbp", "fb@gmail.com", "1111111111");
+            fail("Expected a NullPointerException");
+        } catch (NullPointerException unused) {}
+
+        user = new User("foo", "bar", User.Gender.FEMALE , "myUsername", "fbp", "fb@gmail.com", "1111111111");
+        assertEquals(User.Gender.FEMALE, user.getGender());
+    }
+
+    @Test
+    public void testUsername() {
+        User user = null;
+
+        //null username
+        try {
+            user = new User("foo", "bar", User.Gender.MALE, null, "fbp", "fb@gmail.com", "1111111111");
+            fail("Expected a NullPointerException");
+        } catch (NullPointerException unused) {
+        }
+
+        //empty username
+        try {
+            user = new User("foo", "bar", User.Gender.MALE, "", "fbp", "fb@gmail.com", "1111111111");
+            fail("Expected a IllegalArgumentException");
+        } catch (IllegalArgumentException unused) {
+        }
+
+        //check only spaces
+        try {
+            user = new User("foo", "bar", User.Gender.MALE, "    ", "fbp", "fb@gmail.com", "1111111111");
+            fail("Expected a IllegalArgumentException");
+        } catch (IllegalArgumentException unused) {
+        }
+
+        user = new User("foo", "bar", User.Gender.MALE, "myUsername", "fbp", "fb@gmail.com", "1111111111");
+        assertEquals("myUsername", user.getUsername());
+
+        user = new User("foo", "   bar     ", User.Gender.MALE, "     myUsername ", "fbp", "fb@gmail.com", "1111111111");
+        assertEquals("myUsername", user.getUsername());
+    }
+
+    @Test
+    public void testPhoneNumber(){
+        User user = null;
+
+        //null phone number
+        try {
+            user = new User("foo", "bar", User.Gender.MALE , "myUsername", "fbp", "fb@gmail.com", null);
+            fail("Expected a NullPointerException");
+        } catch (NullPointerException unused) {}
+
+        //check phone number length
+        try {
+            user = new User("foo", "bar", User.Gender.MALE , "myUsername", "fbp", "fb@gmail.com", "123");
+            fail("Expected a IllegalArgumentException");
+        } catch (IllegalArgumentException unused) {}
+        try {
+            user = new User("foo", "bar", User.Gender.MALE , "myUsername", "fbp", "fb@gmail.com", "123456789123456789");
+            fail("Expected a IllegalArgumentException");
+        } catch (IllegalArgumentException unused) {}
+    }
+
+
+
+//    @Test
+//    public void testBookings() {
+//        User user = new User("foo", "bar", User.Gender.FEMALE , "myUsername", "fbp", "fb@gmail.com", "1234567890");
+//    }
 }
