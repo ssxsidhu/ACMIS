4715cb0b46c7ef86e8b20bfd7dc387ca08b88f6d
ssxsidhu <sidhusahil111@gmail.com>
2022-06-23 04:35:21 -0500

    departure reformed

--- app/src/main/java/comp3350/acmis/presentation/FragmentBook.java
+++ app/src/main/java/comp3350/acmis/presentation/FragmentBook.java
@@ -1,15 +1,29 @@
 package comp3350.acmis.presentation;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentResultListener;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TwoLineListItem;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
 import java.util.ArrayList;
 import java.util.Objects;
@@ -27,20 +41,17 @@ public class FragmentBook extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private Location tempDep,tempDes,selectedDeparture,selectedDestination;
+    private Location selectedDeparture,selectedDestination;
+    private DepartureFragment departureFragment;
+    private DestinationFragment destinationFragment;
+    private FloatingActionButton chooseDestination;
+    FragmentManager fragmentManager;
+
 
     public FragmentBook() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BookFragment.
-  */
     //for future use
     public static FragmentBook newInstance(String param1, String param2) {
         FragmentBook fragment = new FragmentBook();
@@ -55,16 +66,21 @@ public class FragmentBook extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
-        if (getArguments() != null) {
-            String mParam1 = getArguments().getString(ARG_PARAM1);
-            String mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        departureFragment = new DepartureFragment();
+        destinationFragment = new DestinationFragment();
+        fragmentManager = getParentFragmentManager();
+
+
+
+
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_book, container, false);
     }
 
@@ -74,80 +90,154 @@ public class FragmentBook extends Fragment {
         ArrayList<Location> locationList = new ArrayList<>();
 
         accessLocations.getLocations(locationList);
+        Bundle args = new Bundle();
+        args.putSerializable("locationList",locationList);
 
-        ArrayAdapter<Location> adapter1 = new ArrayAdapter<>(getActivity(), R.layout.drop_down_menu_item, R.id.menu_text_view, locationList);
-        ArrayAdapter<Location> adapter2 = new ArrayAdapter<>(getActivity(), R.layout.drop_down_menu_item, R.id.menu_text_view, locationList);
-
-        //for dropDown menus
-        AutoCompleteTextView ddDeparture = (AutoCompleteTextView) view.findViewById(R.id.auto_departure);
-        ddDeparture.setThreshold(1);
-        ddDeparture.setAdapter(adapter1);
-        //departure menu
-        TextInputLayout textInputLayout_departure = view.findViewById(R.id.menu_departure);
-        ((AutoCompleteTextView) Objects.requireNonNull(textInputLayout_departure.getEditText())).setOnItemClickListener((adapterView, view1, position, id) -> {
-            if(selectedDestination!=null) {
-                adapter2.add(selectedDestination);
-                tempDes=selectedDestination;
-                selectedDestination=null;
-            }
-            selectedDeparture = adapter1.getItem(position);
-            tempDep=selectedDeparture;
-            adapter1.remove(selectedDeparture);
-            adapter2.remove(selectedDeparture);
-        });
+        FragmentManager fragmentManager = getParentFragmentManager();
 
 
-        //destination menu
-        AutoCompleteTextView ddDestination = (AutoCompleteTextView) view.findViewById(R.id.auto_destination);
-        ddDestination.setThreshold(1);
-        ddDestination.setAdapter(adapter2);
+        FloatingActionButton chooseDeparture = view.findViewById(R.id.choose_departure);
+        chooseDestination = view.findViewById(R.id.choose_destination);
 
-
-        TextInputLayout textInputLayout_destination = view.findViewById(R.id.menu_destination);
-        ((AutoCompleteTextView) Objects.requireNonNull(textInputLayout_destination.getEditText())).setOnItemClickListener((adapterView, view12, position, id) -> {
-            selectedDestination=adapter2.getItem(position);
-            tempDes=selectedDestination;
-            adapter1.remove(selectedDestination);
-            adapter2.remove(selectedDestination);
-            if(selectedDeparture!=null) {
-                tempDep=selectedDeparture;
-                adapter1.add(selectedDeparture);
-                selectedDeparture=null;
-            }
-
-        });
-
-        search(view);
-
-    }
-
-    //search button implementation
-    public void search(View rootView){
-        Button search = rootView.findViewById(R.id.search_button);
-        search.setOnClickListener(view -> {
-            if(tempDes!=null && tempDep!=null) {
-                sendData();
-            }
-            else{
-                if(tempDep == null){
-                    Messages.snackBar(view,"Please select departure");
-                }
-                else {
-                    Messages.snackBar(view,"Please select destination");
-                }
+        chooseDeparture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseDeparture.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ADC6FF")));
+                departureFragment.setArguments(args);
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_book, departureFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
             }
         });
 
 
+        chooseDestination.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseDestination.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ADC6FF")));
+                destinationFragment.setArguments(args);
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_book, destinationFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+
+        TextView setAirport = view.findViewById(R.id.airport);
+        TextView setCity = view.findViewById(R.id.city);
+
+
+        setAirport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAirport.setVisibility(View.INVISIBLE);
+                setCity.setVisibility(View.INVISIBLE);
+                chooseDeparture.show();
+                chooseDeparture.performClick();
+            }
+        });
+
+        fragmentManager.setFragmentResultListener("selectedDeparture", getViewLifecycleOwner(), new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                selectedDeparture = (Location) result.getSerializable(requestKey);
+                setAirport.setText(selectedDeparture.getAirport());
+                setCity.setText(String.format("%s,%s", selectedDeparture.getCity(), selectedDeparture.getCountry()));
+                chooseDeparture.hide();
+                setAirport.setVisibility(View.VISIBLE);
+                setCity.setVisibility(View.VISIBLE);
+                chooseDestination.setEnabled(true);
+                chooseDestination.performClick();
+            }
+        });
+
+        fragmentManager.setFragmentResultListener("selectedDestination", getViewLifecycleOwner(), new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                selectedDestination = (Location) result.getSerializable(requestKey);
+            }
+        });
+
+
+
+
+
+
+//        ArrayAdapter<Location> adapter1 = new ArrayAdapter<>(getActivity(), R.layout.drop_down_menu_item, R.id.menu_text_view, locationList);
+//        ArrayAdapter<Location> adapter2 = new ArrayAdapter<>(getActivity(), R.layout.drop_down_menu_item, R.id.menu_text_view, locationList);
+//
+//        //for dropDown menus
+//        AutoCompleteTextView ddDeparture = (AutoCompleteTextView) view.findViewById(R.id.auto_departure);
+//        ddDeparture.setThreshold(1);
+//        ddDeparture.setAdapter(adapter1);
+//        //departure menu
+//        TextInputLayout textInputLayout_departure = view.findViewById(R.id.menu_departure);
+//        ((AutoCompleteTextView) Objects.requireNonNull(textInputLayout_departure.getEditText())).setOnItemClickListener((adapterView, view1, position, id) -> {
+//            if(selectedDestination!=null) {
+//                adapter2.add(selectedDestination);
+//                tempDes=selectedDestination;
+//                selectedDestination=null;
+//            }
+//            selectedDeparture = adapter1.getItem(position);
+//            tempDep=selectedDeparture;
+//            adapter1.remove(selectedDeparture);
+//            adapter2.remove(selectedDeparture);
+//        });
+//
+//
+//        //destination menu
+//        AutoCompleteTextView ddDestination = (AutoCompleteTextView) view.findViewById(R.id.auto_destination);
+//        ddDestination.setThreshold(1);
+//        ddDestination.setAdapter(adapter2);
+//
+//
+//        TextInputLayout textInputLayout_destination = view.findViewById(R.id.menu_destination);
+//        ((AutoCompleteTextView) Objects.requireNonNull(textInputLayout_destination.getEditText())).setOnItemClickListener((adapterView, view12, position, id) -> {
+//            selectedDestination=adapter2.getItem(position);
+//            tempDes=selectedDestination;
+//            adapter1.remove(selectedDestination);
+//            adapter2.remove(selectedDestination);
+//            if(selectedDeparture!=null) {
+//                tempDep=selectedDeparture;
+//                adapter1.add(selectedDeparture);
+//                selectedDeparture=null;
+//            }
+//
+//        });
+//
+//        search(view);
+//
+//    }
+//
+//    //search button implementation
+//    public void search(View rootView){
+//        Button search = rootView.findViewById(R.id.search_button);
+//        search.setOnClickListener(view -> {
+//            if(tempDes!=null && tempDep!=null) {
+//                sendData();
+//            }
+//            else{
+//                if(tempDep == null){
+//                    Messages.snackBar(view,"Please select departure");
+//                }
+//                else {
+//                    Messages.snackBar(view,"Please select destination");
+//                }
+//            }
+//        });
+
+
     }
 
-    private void sendData(){
-        Intent i = new Intent(getActivity().getBaseContext(),SearchResults.class);
-        i.putExtra("selectedDeparture", tempDep);
-        i.putExtra("selectedDestination",tempDes);
-        getActivity().startActivity(i);
-
-    }
+//    private void sendData(){
+//        Intent i = new Intent(getActivity().getBaseContext(),SearchResults.class);
+//        i.putExtra("selectedDeparture", tempDep);
+//        i.putExtra("selectedDestination",tempDes);
+//        getActivity().startActivity(i);
+//
+//    }
 
 
 }
