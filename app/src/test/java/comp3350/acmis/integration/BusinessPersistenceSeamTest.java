ebaeeb34bf042778c7a9a37121f33c716ed26bf9
vRuShIl13 <patelvk1@myumanitoba.ca>
2022-07-27 22:50:14 -0500

    continuation of integration testing

--- app/src/test/java/comp3350/acmis/integration/BusinessPersistenceSeamTest.java
+++ app/src/test/java/comp3350/acmis/integration/BusinessPersistenceSeamTest.java
@@ -1,7 +1,34 @@
 package comp3350.acmis.integration;
 
 
-public class BusinessPersistenceSeamTest {
+import org.junit.Test;
+
+import java.util.ServiceConfigurationError;
+
+import comp3350.acmis.application.Main;
+import comp3350.acmis.application.Services;
+import comp3350.acmis.business.AccessBookings;
+
+public class BusinessPersistenceSeamTest  {
+    private static String dnName;
+
+    public BusinessPersistenceSeamTest(String typeOfDb){
+        dnName = typeOfDb;
+    }
 
 
+    @Test
+    public void testAccessBookings(){
+        AccessBookings testAccess;
+
+
+        //resetting persistence
+        Services.closeDataAccess();
+
+        System.out.println("Starting Integration test of AccessBooking to persistence");
+
+        Services.createDataAccess(Main.dbName);
+
+        testAccess = new AccessBookings("braico");
+    }
 }
